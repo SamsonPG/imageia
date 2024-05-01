b0653dd9e6a750b9227cf1be11266902419e340c
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-05-02 00:32:43 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default clerkMiddleware();
+
+export default authMiddleware({
+  publicRoutes:['/api/webhooks/clerk'],
+  // ignoredRoutes: ["/api/webhooks(.*)"],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
